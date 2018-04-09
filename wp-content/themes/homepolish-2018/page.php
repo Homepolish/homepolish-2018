d0682762b40d86c3a79f7340a9d7786861722888
Evan Rose <work@evanrose.com>
2018-04-09 12:43:00 -0400

    Updated page template

--- wp-content/themes/homepolish-2018/page.php
+++ wp-content/themes/homepolish-2018/page.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
 
+<!-- Fix this ID-->
+
+<div id="frequently-asked-questions">
+
+	<?php
+		the_title( '<h1 class="page-title">', '</h1>' );
+		the_content();
+	?>
+</div>
 
 <?php get_footer(); ?>
