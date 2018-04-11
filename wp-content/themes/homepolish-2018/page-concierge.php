972a72262e71a406925499029486dd1e53bae069
Evan Rose <work@evanrose.com>
2018-04-11 16:28:59 -0400

    Brands for concierge

--- wp-content/themes/homepolish-2018/page-concierge.php
+++ wp-content/themes/homepolish-2018/page-concierge.php
@@ -1,10 +1,6 @@
 <?php get_header(); ?>
 
-<!-- to do brands code mobile and desktop, with repeater and brand post type -->
-
-<!-- no pages have this? -->
 <div class="concierge-page">
-
 
 <!-- concierge-hero -->
 
@@ -21,10 +17,7 @@
 	</div>
 </div><!-- ./concierge-hero -->
 
-
 <!-- hiw -->
-
-<!-- To do - on live site this is HTML -->
 
 <?php the_field( 'free_html' ); ?>
 
@@ -60,137 +53,61 @@
 	</div>
 </div><!-- ./make-ordering-breeze -->
 
-
+<!-- testimonials block -->
 
 <?php get_template_part('templates/block', 'testimonials'); ?>
 
+<!-- testimonials block -->
 
+<!-- brands -->
 
 <div class="concierge-mobile">
-<div class="client-logos partners">
-<div class="partners-header">
-<h2>
-<?php the_field( 'pb_title' ); ?>
-</h2>
-<p class="description formatted-copy--mobile">
-<?php the_field( 'pb_copy' ); ?>
-</p>
-<h5 class="intro">
-<?php the_field( 'pb_subtitle' ); ?>
-</h5>
-</div>
-<div class="owl-carousel" data-owl-carousel>
+	<div class="client-logos partners">
+		<div class="partners-header">
+			<h2><?php the_field( 'pb_title' ); ?></h2>
+			<p class="description formatted-copy--mobile"><?php the_field( 'pb_copy' ); ?></p>
+			<h5 class="intro"><?php the_field( 'pb_subtitle' ); ?></h5>
+		</div>
+		<div class="owl-carousel" data-owl-carousel>
+			<div class="logos-card">
+				<?php 
 
+					$i = 1;
+					$brands = get_field( 'pb_brand_images' );
+					foreach( $brands as $value ) { ?>
 
-<?php 
+						<?php $brand = $value['pb_brand_image']; ?>
 
-	echo '<div class="logos-card">'
+						<div class="logo-img-container">
+							<img alt="The logo of Homepolish partner <?php echo get_the_title( $brand->ID ); ?>" class="logo-img" src="<?php echo get_the_post_thumbnail_url( $brand->ID ); ?>">
+						</div>
 
-	$i = 1;
-	$brands = get_field( 'pb_brand_images' );
-	var_dump( $brands );
-	exit;
-	
-	foreach( $brands as $brand ) {
-
-		get_the_post_thumbnail_url( ;
-	}
-
-	echo '</div>'
-?>
-
-
-<div class="logos-card">
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner West Elm" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/west_elm.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Pottery Barn" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/pottery_barn.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Anthropologie" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/anthropologie.png">
-</div>
-</div>
-<div class="logos-card">
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Blu Dot" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/blu_dot.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Serena &amp; Lily" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/serena_lily.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner CB2" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/cb2.png">
-</div>
-</div>
-<div class="logos-card">
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Schoolhouse" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/schoolhouse.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Design Within Reach" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/design_within_reach.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Arteriors" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/arteriors.png">
-</div>
-</div>
-<div class="logos-card">
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Surya" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/surya.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Lulu &amp; Georgia" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/lulu_georgia.png">
-</div>
-<div class="logo-img-container">
-<img alt="The logo of Homepolish partner Joybird" class="logo-img" src="https://www.homepolish.com/cdn/concierge/partners/joy_bird.png">
-</div>
-</div>
-</div>
-</div>
-
+						<?php if ( $i % 3 == 0 ) { ?>
+							</div>
+							<div class="logos-card">
+						<?php } ?>
+					
+				<?php } ?>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="concierge-desktop">
-<div class="client-logos partners">
-<div class="partners-header">
-<h2 class="title">
-<?php the_field( 'pb_title' ); ?>
-</h2>
-<p class="description formatted-copy--tablet-desktop">
-<?php the_field( 'pb_copy' ); ?>
-</p>
-<h5 class="intro">
-<?php the_field( 'pb_subtitle' ); ?>
-</h5>
-</div>
-<div class="logos-desktop">
-
-<?php $image = get_field( 'pb_image' ); ?>
-
-<img class="logos-desktop-img" src="<?php echo $image['url']; ?>">
-</div>
-</div>
-
-</div>
-
+	<div class="client-logos partners">
+		<div class="partners-header">
+			<h2 class="title"><?php the_field( 'pb_title' ); ?></h2>
+			<p class="description formatted-copy--tablet-desktop"><?php the_field( 'pb_copy' ); ?></p>
+			<h5 class="intro"><?php the_field( 'pb_subtitle' ); ?></h5>
+		</div>
+		<div class="logos-desktop">
+			<?php $image = get_field( 'pb_image' ); ?>
+			<img class="logos-desktop-img" src="<?php echo $image['url']; ?>">
+		</div>
+	</div>
+</div><!-- ./brands -->
 
 <?php get_template_part('templates/block', 'signup'); ?>
 
-<!-- 
-<div class="concierge-book-now-cta">
-<div class="book-now-cta">
-<h5 class="cta-header">
-Get started with a complimentary consultation
-</h5>
-<h3 class="cta-content">
-Let us select the perfect design team for you.
-</h3>
-<a class="cta-button" data-track-book-now="true" data-button-id="inline_cta" href="/start">Sign Up</a>
 </div>
--->
-
-
-</div>
-
-
-
 
 <?php get_footer(); ?>
