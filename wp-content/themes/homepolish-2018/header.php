5620f16d7ffb1a3731bcb482f9197f1724125c54
Evan Rose <work@evanrose.com>
2018-04-18 12:25:44 -0400

    Remote Test

--- wp-content/themes/homepolish-2018/header.php
+++ wp-content/themes/homepolish-2018/header.php
@@ -1,4 +1,4 @@
-<!-- documentation test -->
+<!-- multi-remote -->
 
 <!DOCTYPE html>
 <html itemscope itemtype="http://schema.org/<?php echo $page_type; ?>" lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
