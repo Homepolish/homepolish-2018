f77d3e43e6106e193852fa337f9f048065dd1582
Evan Rose <work@evanrose.com>
2018-04-19 10:15:25 -0400

    Remove emoji code

--- wp-content/themes/homepolish-2018/functions.php
+++ wp-content/themes/homepolish-2018/functions.php
@@ -116,3 +116,12 @@ function hp_image_styles( $mobile_desktop_array ) {
 
 	return $styles;	
 }
+
+/**
+@ Remove WordPress Emoji Code
+*/
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
