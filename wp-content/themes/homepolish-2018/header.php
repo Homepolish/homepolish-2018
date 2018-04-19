46dd5fa441422d9e35704530617b2c2726249a63
Evan Rose <work@evanrose.com>
2018-04-19 10:29:13 -0400

    Cleaned up meta values

--- wp-content/themes/homepolish-2018/header.php
+++ wp-content/themes/homepolish-2018/header.php
@@ -1,29 +1,18 @@
 <!DOCTYPE html>
-<html itemscope itemtype="http://schema.org/<?php echo $page_type; ?>" lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
-<title></title>
+<html itemscope itemtype="http://schema.org/PAGETYPE?" lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+
+<!-- header.php values -->
+<link href="<?php echo get_template_directory_uri(); ?>/assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<link href="<?php echo get_template_directory_uri(); ?>/assets/homepolish_logo.png" rel="apple-touch-icon">
 <meta name="csrf-param" content="authenticity_token" />
-<meta name="csrf-token" content="<?php echo $csrf_token; ?>" />
-<meta content="" name="description"/>
+<meta name="csrf-token" content="CSRF-TOKEN?" />
 <meta content="Homepolish" itemprop="name">
 <meta content="127512634049491" property="fb:app_id">
-<meta content="website" property="og:type">
-<meta content="Homepolish: Seamless and Personal Interior Design" property="og:title">
-<meta content="Premium interior design services in-home and online nationwide. From decorating rooms to renovating offices, our designers help you get any job done in style." property="og:description">
-<meta content="https://www.homepolish.com/cdn/homepage/meta-images/og.jpg" property="og:image">
-<meta content="https://homepolish.com/" property="og:url">
-<meta content="Homepolish" property="og:site_name">
-<meta content="en_US" property="og:locale">
-<meta content="summary_large_image" name="twitter:card">
-<meta content="Homepolish: Seamless and Personal Interior Design" name="twitter:title">
-<meta content="Premium interior design services in-home and online nationwide. From decorating rooms to renovating offices, our designers help you get any job done in style." name="twitter:description">
-<meta content="https://www.homepolish.com/cdn/homepage/meta-images/twitter.jpg" name="twitter:image">
-<meta content="Living Room in Williamsburg Bachelor Pad" name="twitter:image:alt">
-<meta content="@homepolish" name="twitter:site">
-<meta content="@homepolish" name="twitter:creator">
-<link rel="shortcut icon" type="image/x-icon" href="https://www.homepolish.com/assets/favicon-e848a316df83ffd48bc2a038b2e4d38988f99ab8ebe9b82f3cf9aaa6029d6845.ico" />
-<link href="<?php echo get_template_directory_uri(); ?>/assets/homepolish_logo.png" rel="apple-touch-icon">
+<!-- /header.php values -->
 
+<!-- wp_head -->
 <?php wp_head(); ?>
+<!-- /wp_head -->
 
 </head>
 
