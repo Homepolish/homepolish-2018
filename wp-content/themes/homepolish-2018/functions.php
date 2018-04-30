84b2ba91101aa9b7f1efc0238c9155a3492dc555
Evan Rose <work@evanrose.com>
2018-04-30 12:03:49 -0400

    Remove Yoast SEO Json

--- wp-content/themes/homepolish-2018/functions.php
+++ wp-content/themes/homepolish-2018/functions.php
@@ -120,3 +120,14 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
 remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+/** 
+@Remove Yoast SEO JSON schema
+*/
+
+function hp_remove_yoast_json( $data ) {
+    
+    $data = array();
+    return $data;
+  }
+  add_filter( 'wpseo_json_ld_output', 'hp_remove_yoast_json', 10, 1 );
