03286140cb3986c84ff56a42d9c053b8b4098767
Evan Rose <work@evanrose.com>
2018-06-01 11:36:53 -0400

    Header fix

--- wp-content/themes/thevoux-wp/header.php
+++ wp-content/themes/thevoux-wp/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- homepolishwp --><!DOCTYPE html>
 <html itemscope itemtype="http://schema.org/<?php echo hp_page_meta()['page_type']; ?>" lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
 <head>
 	<link href="<?php echo get_template_directory_uri(); ?>/assets-2018/favicon.ico" rel="shortcut icon" type="image/x-icon" />
