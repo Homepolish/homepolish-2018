b71d826ad5712e47139a928c4d6d77a034f14c66
Evan Rose <work@evanrose.com>
2018-06-08 14:03:35 -0400

    Staging test

--- wp-content-2018/themes/hp-2018/header.php
+++ wp-content-2018/themes/hp-2018/header.php
@@ -3,7 +3,10 @@
 	// This is a stopgap as we change the URL scheme. Once we go live with /mag, remove 6/8/18 EAR
 	if( ! strpos( site_url(), 'staging' ) ) {
 
-		echo '<!-- staging -->';
+		echo 'prod or dev';
+	}
+	else {
+		echo 'staging!';
 	}
 ?>
 
