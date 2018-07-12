d37d89ba783e959aefffde4fa3a5eea4daf4e205
Evan Rose <work@evanrose.com>
2018-07-12 16:32:54 -0400

    Removed DNS prefetch

--- wp-content-2018/themes/thevoux-wp/functions.php
+++ wp-content-2018/themes/thevoux-wp/functions.php
@@ -287,6 +287,7 @@ remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'feed_links', 2 ); 
 remove_action( 'wp_head', 'rsd_link' ); 
 remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
 
 /** 
 @ Remove Yoast SEO JSON schema
