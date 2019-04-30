efa01504af9346951d9ef818c613f1251cb28fd2
Rick Szatkowski <therick@the-rick.com>
2019-04-30 16:33:24 -0400

    Additional changes to about 2

--- wp-content-2018/themes/thevoux-wp/page-about_ii.php
+++ wp-content-2018/themes/thevoux-wp/page-about_ii.php
@@ -33,100 +33,16 @@
 <!-- our-promise -->
 
 <div class="our-promise">
-
-    <?php 
-        $row = array();
-        $row = get_field( '2x2_row' );
-        foreach( $row as $key => $value ) {
-        ?>
-
-        <div class="our-promise__section">
-            <h5 class="section__header"><?php echo $value['2xl_title']; ?></h5>
-            <p class="section__body formatted-copy--mobile">
-                <?php echo $value['2xl_copy']; ?>
-            </p>
-            <p class="section__body formatted-copy--tablet-desktop">
-                <?php echo $value['2xl_copy']; ?>
-            </p>
-        </div>
-        <div class="our-promise__section">
-            <h5 class="section__header"><?php echo $value['2xr_title']; ?></h5>
-            <p class="section__body formatted-copy--mobile">
-                <?php echo $value['2xr_copy']; ?>
-            </p>
-            <p class="section__body formatted-copy--tablet-desktop">
-                <?php echo $value['2xr_copy']; ?>
-            </p>
-        </div>
-
-        <? } 
-   ?>
+    <div class="our-promise__section" style="width: 100%;">
+        <h5 class="section__header"><?php the_field( 'ctr_title' ); ?></h5>
+        <p class="section__body formatted-copy--mobile">
+            <?php the_field( 'ctr_copy' ); ?>
+        </p>
+        <p class="section__body formatted-copy--tablet-desktop">
+            <?php the_field( 'ctr_copy' ); ?>
+        </p>
+    </div>
 </div><!-- ./our-promise -->
-
-
-<!-- why-homepolish -->
-
-<div class="why-homepolish">
-    <h5 class="why-homepolish__title">
-        <?php the_field( 'title' ); ?>
-    </h5>
-
-    <h2 class="why-homepolish__header formatted-copy--mobile">
-        <?php the_field( 'subtitle' ); ?>
-    </h2>
-
-    <h2 class="why-homepolish__header formatted-copy--tablet-desktop">
-        <?php the_field( 'subtitle' ); ?>
-    </h2>
-
-    <?php
-        $i = 1;
-        $row = array();
-        $row = get_field( 'wch_row' );
-
-        foreach( $row as $value ) {
-        ?>
-
-        <div class="why-homepolish__section">
-
-            <?php
-
-                $args = array(
-                    //$value['wchr_mobile_image'],
-                    $value['wchr_image'],
-                    $value['wchr_image'],
-                    '.landing-pages--about-us .why-homepolish .section__image.section__image--' . $i
-                );
-                echo hp_image_styles( $args );
-            ?>
-
-            <div class="section__image section__image--<?php echo $i++; ?>"></div>
-            <div class="section__text">
-                <div class="text__wrapper">
-                    <h4 class="text__header" itemprop="specialty">
-                        <?php echo $value['wchr_title']; ?>
-                    </h4>
-                    <p class="text__body formatted-copy--mobile">
-                        <?php echo $value['wchr_copy']; ?>
-                    </p>
-                    <p class="text__body formatted-copy--tablet-desktop">
-                        <?php echo $value['wchr_copy']; ?>
-                    </p>
-
-                    <?php if( $value['wchr_link'] ) { ?>
-                        <h5 class="cta__link">
-                            <a href="<?php echo $value['wchr_link']['url']; ?>" class="cta-link">
-                                <span class="cta-link-text"><?php echo $value['wchr_link']['title']; ?></span>
-                                <span class="v1-icon-caret-right"></span>
-                            </a>
-                        </h5>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-
-    <?php } ?>
-</div><!-- ./why-homepolish -->
 
 <!-- ctas -->
 
